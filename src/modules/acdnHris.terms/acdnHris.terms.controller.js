1f7b035e6dd82685b1aa1b5d43bdfc487105eb7e
Nick Pe <springate.nick@gmail.com>
2016-09-21 19:14:22 +0300

    Filled terms.template

--- src/modules/acdnHris.terms/acdnHris.terms.controller.js
+++ src/modules/acdnHris.terms/acdnHris.terms.controller.js
@@ -4,11 +4,16 @@
         .module("acdnHris.terms")
         .controller("TermsCtrl", TermsCtrl);
 
-    function TermsCtrl($ionicHistory) {
+    // function TermsCtrl($ionicHistory, CordovaService $ionicPlatform, $cordovaEmailComposer, $error) {
+    function TermsCtrl($ionicHistory, CordovaService) {
         this.test = "Terms page";
 
         this.backButton = function() {
             $ionicHistory.goBack(-1);
+        };
+
+        this.sendMail = function() {
+            CordovaService.sendEmail();
         };
     }
 
--- src/modules/services/acdnHris.cordova.service.js
+++ src/modules/services/acdnHris.cordova.service.js
@@ -2,10 +2,43 @@
 
     angular
         .module("acdnHris")
-        .service("Cordova", CordovaFactory);
+        .service("CordovaService", CordovaService);
 
-    function CordovaFactory() {
+    function CordovaService(
+        $ionicPlatform,
+        $cordovaEmailComposer,
+        $error,
+        $ionicPopup
+    ) {
         var self = this;
+
+        //Function fot sending email
+        self.sendEmail = function() {
+            var mailTemplate = {
+                to: 'info@agedcaredn.com.au'
+            };
+
+            $ionicPlatform.ready(function() {
+                $cordovaEmailComposer.isAvailable().then(
+                    function() {
+                        return $cordovaEmailComposer.open(mailTemplate);
+                    },
+                    function() {
+                        $ionicPopup.alert({
+                            title: 'You cant send email',
+                            template: 'Please make sure you have configured an email account and have installed an email app'
+                        })
+                        .then(function(res) {
+                            console.log(res);
+                            $error("You cant send email. Please make sure you have configured an email account and have installed an email app.");
+                        });
+                    }
+                ).catch(function(err) {
+                    $error("Error with sending email \n", err);
+                });
+            });
+
+        };
     }
 
 })();
