787acdef5b04581dfe68ac7582baa25ec335d01e
Nick Pe <springate.nick@gmail.com>
2016-09-29 19:46:18 +0300

    Porfolio commit

--- src/modules/portfolio/portfolio.controller.js
+++ src/modules/portfolio/portfolio.controller.js
@@ -5,11 +5,48 @@
         .controller("PortfolioCtrl", PortfolioCtrl);
 
     function PortfolioCtrl(
+        $scope,
+        $ionicModal,
         UserService
     ) {
         var vm = this;
+        var modalInstance = null;
+        var modalTemplates =  {
+            education: "templates/portfolio/education.modal.template.html",
+            experience: "templates/portfolio/experience.modal.template.html"
+        };
 
         vm.userInfo = UserService.userInfo;
+        vm.showModal = showModal;
+
+        $scope.hideModal = hideModal;
+        $scope.consoleQW = consoleQW;//Delete
+        $scope.educationData = {
+            org: "",
+            course: "",
+            date: ""
+        };
+
+        function showModal(name) {
+            $ionicModal.fromTemplateUrl(modalTemplates[name], {
+                animation: 'slide-in-up',
+                scope: $scope
+            }).then(function(modal) {
+                modal.show();
+                modalInstance = modal;
+            });
+        }
+
+        function hideModal() {
+            modalInstance.hide();
+            modalInstance = null;
+        }
+
+        function consoleQW() {//Delete
+            console.log($scope.educationData);
+        }
+
+
     }
 
 })();
